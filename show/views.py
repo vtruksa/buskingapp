4eac39bc95bdb94bfebf3467a8ceeb2fe51a6e91
vtruksa <washeek.t@gmail.com>
2023-11-27 11:55:33 +0100

    click detection inside the folium map is now working, created a times.txt file to store the allowed times of playing

--- show/views.py
+++ show/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from branca.element import JavascriptLink
 
 import folium, clipboard
 
@@ -6,10 +7,19 @@ import folium, clipboard
 def manageSpots(request):
     m = folium.Map(location=(50.0755, 14.4378))
     #m.add_child(folium.ClickForLatLng(format_str='"[" + lat + "," + lng + "]"', alert=False))
-    m.add_child(folium.ClickForMarker(popup="<b>Lat:</b> ${lat}<br /><b>Lon:</b> ${lng}<br><button class='btn btn-primary'>Přidat</button>"))
+    m.add_child(folium.ClickForMarker(popup="<b>Lat:</b> ${lat}<br /><b>Lon:</b> ${lng}<br>"))
+    m.add_child(folium.ClickForLatLng(alert=False))
 
+    m.get_root().html.add_child(JavascriptLink('../static/js/spot_manage_iframe.js'))
+
+    times = open('static/times.txt', 'r').read()
+    times = times.split('\n')
+    t_arr = []
+    for t in times:
+        t_arr.append(t)
     context={
-        'map':m._repr_html_()
+        'map':m._repr_html_(),
+        'times':t_arr
     }
 
-    return render(request, 'spot_manage.html', context)
+    return render(request, 'spot_manage.html', context) 
--- static/js/spot_manage.js
+++ static/js/spot_manage.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
     console.log('ready')
+
+    
+    //setup_map_btn()
 })
 
+function add_click() {
+    console.log('click')
+}
